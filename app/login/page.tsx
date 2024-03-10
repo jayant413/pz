adcc0dd99a63ef614a6678ee1f86345efde5597b
jayant413 <jayantsawarkar4103@gmail.com>
2024-03-10 10:09:56 +0530

    feat: setup

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+const Login = () => {
+  return (
+    <div>
+      <header>
+        <div className="logo">
+          <img src="img/Logo.png" alt="logo" />
+        </div>
+        <div>&nbsp;</div>
+      </header>
+      <main className="relative">
+        <div className="background-img "></div>
+        <form
+          action=""
+          className="bg-blue-500 w-[30rem] h-[30rem] 
+          absolute rounded-xl right-[20vw] top-[10vh]"
+        >
+          <div></div>
+        </form>
+        <div className="gray-background"></div>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
