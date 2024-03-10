96acedf930693f581664759cb709980fea66d1e4
jayant413 <jayantsawarkar4103@gmail.com>
2024-03-10 10:22:20 +0530

    feat: Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,13 +8,40 @@ const Login = () => {
         <div>&nbsp;</div>
       </header>
       <main className="relative">
-        <div className="background-img "></div>
+        <div className="background-img bg-[url(/img/bg.jpg)]"></div>
         <form
           action=""
-          className="bg-blue-500 w-[30rem] h-[30rem] 
-          absolute rounded-xl right-[20vw] top-[10vh]"
+          className="bg-white text-gray-600 w-[30rem] h-[30rem]  flex flex-col gap-y-8 
+          absolute rounded-xl right-[20vw] top-[10vh] justify-center items-center"
         >
-          <div></div>
+          <div className="flex justify-center items-center text-xl uppercase">
+            {" "}
+            <a href="/login" className="text-gray-400">
+              Register
+            </a>
+            <span className="mx-5">|</span>
+            <a href="/login">Login</a>
+          </div>
+          <div className="flex flex-col w-[80%] gap-y-3">
+            <input
+              type="text"
+              placeholder="Username"
+              className="p-2 bg-transparent border-b-2 border-gray-500 outline-none"
+            />
+            <input
+              type="text"
+              placeholder="password"
+              className="p-2 bg-transparent border-b-2 border-gray-500 outline-none"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-gray-200 hover:bg-gray-400 p-3 rounded-xl hover:text-white"
+            >
+              Submit
+            </button>
+          </div>
         </form>
         <div className="gray-background"></div>
       </main>
